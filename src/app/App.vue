994375cef67b6909d32cca29d6b6f6719429aae1
lewis2101 <bauka2101bux@gmail.com>
2025-08-17 22:53:45 +0500

    remove unusable files

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,14 +1,4 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-
-// onMounted(() => {
-//   fetch("https://api.pexels.com/v1/search?query=Природа&per_page=10&size=medium&locale=ru", {
-//     headers: {
-//       "Authorization": import.meta.env.VITE_PEXELS_API_KEY,
-//     }
-//   })
-// })
-
 </script>
 
 <template>
